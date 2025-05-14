d8d10d96463016b2864a77fccbd88e314a450a5a
falcom5977 <guigandrades@gmail.com>
2025-05-13 21:53:52 -0300

    Add files via upload

---
+++ exercicio 2.py
@@ -0,0 +1,7 @@
+n = int(input("valor "))
+r = int(input("valor "))
+a1 = int(input("valor "))
+
+for i in range(a1,n):
+    an = a1 + (n-1) * r
+print(an)
---
+++ exercicio 3.py
@@ -0,0 +1,7 @@
+n = int(input("valor "))
+r = int(input("valor "))
+a1 = int(input("valor "))
+
+for i in range(a1,n):
+    an = a1 * (r ** (n-1))
+print(an)
