9aa5815976a5091fcdc18be3ec275a2a29a4802b
falcom5977 <guigandrades@gmail.com>
2025-05-13 21:34:36 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
-yes = True
-usuarios = {}
+def calcular_juros_compostos_for(P, i, n):
+    M = P
 
-while yes != False:
-    print("menu\n")
-    print("1. Cadastrar\n")
-    print("2.login\n")
-    menu= int(input("qual vai ser sua escolha?: "))
-    if menu == 1:
-        new_user = input("qual é o usuario?: ")
-        new_senha = input("qual é a senha?:")
-        with open("dados.txt","a") as f:
-            f.write(f"{new_user}:{new_senha}\n")
-    elif menu ==2:
-        tentativa = 3
-        while tentativa >= 1:
-            user_guess = input("qual é o usuario?: ")
-            senha_guess = input("Qaul e a shenha?: ")
-            with open("dados.txt", "r") as f:
-                for linha in f:
-                    usuario,senha = linha.strip().split(":")
-                    usuario[usuario] = senha
-            if user_guess in usuarios and usuarios[user_guess] == senha_guess:
-                print("\nBem-vindo novamente")
-                tentativa -= 1
+
+    for periodo in range(n):
+        M += M * (i / 100)
+
+P = int(input("Qual valor você deseja investir"))
+i = 0.05
+n = int(input("Quanto tempo você deseja investir"))
+
+
+montante = calcular_juros_compostos_for(P, i, n)
+
+print(f"Montante final após {n} períodos: R$ {montante:.2f}")
